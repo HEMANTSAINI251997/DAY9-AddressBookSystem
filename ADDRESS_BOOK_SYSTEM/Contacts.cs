f3a02b545f72ad731fe3c86b3f629f168133c5a0
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-07-14 14:42:56 +0530

    [HEMANT]Add: Edit the detailf of Address Book

--- ADDRESS_BOOK_SYSTEM/Contacts.cs
+++ ADDRESS_BOOK_SYSTEM/Contacts.cs
@@ -9,9 +9,13 @@ namespace ADDRESS_BOOK_SYSTEM
 {
     public class Contacts
     {
+        List<AddressBook> list = new List<AddressBook>();
         public void CheckContacts()
         {
-            List<AddressBook> list = new List<AddressBook>();
+           
+            Console.WriteLine("Enter no.of details want to print:");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
 
             {
                 AddressBook address = new AddressBook();
@@ -38,6 +42,63 @@ namespace ADDRESS_BOOK_SYSTEM
                 Console.WriteLine("\n" + "FirstName   = " + address.firstname + "\n" + "Second Name = " + address.lastname + "\n" + "Address     = " + address.address + "\n"
                     + "City        = " + address.city + "\n" + "State       = " + address.state + "\n" + "PhoneNumber = " + address.phoneNo + "\n" +
                     "Zip Code    = " + address.postcode + "\n" + "Country     = " + address.country + "\n" + "Email       = " + address.email);
+                Console.WriteLine("Edit by using First Name: ");
+            }
+            String name = Console.ReadLine();
+            foreach (var data in list)
+            {
+                AddressBook address = new AddressBook();
+                if (data.firstname == name)
+                {
+                    Console.WriteLine("Enter option to edit:\n1.First Name\n2.Second Name\n3.Address\n4.city\n5.state\n6.phone number\n7.email\n8.Exit");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+
+                    {
+                        case 1:
+                            Console.WriteLine("Enter first name ");
+                            data.firstname = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter last name ");
+                            data.lastname = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter address");
+                            data.address = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter city");
+                            data.city = Console.ReadLine();
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter state");
+                            data.state = Console.ReadLine();
+                            break;
+                        case 6:
+                            Console.WriteLine("Enter phone number");
+                            data.phoneNo = Console.ReadLine();
+                            break;
+                        case 7:
+                            Console.WriteLine("Enter email");
+                            data.email = Console.ReadLine();
+                            break;
+
+                        default:
+                            Console.WriteLine("Entered Invalid Option");
+                            break;
+                    }
+                    Console.WriteLine("\n" + "FirstName   = " + data.firstname + "\n" + "Second Name = " + data.lastname + "\n" + "Address     = " + data.address + "\n"
+                        + "City        = " + data.city + "\n" + "State       = " + data.state + "\n" + "PhoneNumber = " + data.phoneNo + "\n" +
+                        "Zip Code    = " + data.postcode + "\n" + "Country     = " + data.country + "\n" + "Email       = " + data.email);
+
+                }
+                else
+                {
+                    Console.WriteLine("Given first name is not found");
+                    return;
+                }
+
             }
         }
     }
