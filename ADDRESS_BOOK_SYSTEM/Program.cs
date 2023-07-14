e41119c3e3143271c4b5a3b9a277ff10e280df94
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-07-14 13:47:44 +0530

    [HEMANT]Add:Ability to creat Contacts in Address Book

---
+++ ADDRESS_BOOK_SYSTEM/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADDRESS_BOOK_SYSTEM
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO ADDRESS BOOK");
+            AddressBook addressbook = new AddressBook();
+            addressbook.Checkaddressbook();
+        }
+    }
+}
